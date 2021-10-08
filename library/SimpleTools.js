d069068373c79c634ee16576e8b31ceb2ffdebac
windeng <mis_tletoe@foxmail.com>
2021-10-08 16:22:00 +0800

    update

--- library/SimpleTools.js
+++ library/SimpleTools.js
@@ -1,76 +1,94 @@
 async function Sleep(sleepSecs) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve()
-        }, sleepSecs * 1000)
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, sleepSecs * 1000)
+  })
 }
- 
+
 async function WaitUntil(conditionFunc, sleepSecs) {
-    sleepSecs = sleepSecs || 1
-    return new Promise((resolve, reject) => {
-        if (conditionFunc()) resolve()
-        let interval = setInterval(() => {
-            if (conditionFunc()) {
-                clearInterval(interval)
-                resolve()
-            }
-        }, sleepSecs * 1000)
-    })
+  sleepSecs = sleepSecs || 1
+  return new Promise((resolve, reject) => {
+    if (conditionFunc()) resolve()
+    let interval = setInterval(() => {
+      if (conditionFunc()) {
+        clearInterval(interval)
+        resolve()
+      }
+    }, sleepSecs * 1000)
+  })
 }
- 
+
 // GM_xmlhttpRequest
-function Request(url, opt={}) {
-	Object.assign(opt, {
-		url,
-		timeout: 2000,
-		responseType: 'json'
-	})
- 
-	return new Promise((resolve, reject) => {
-		/*
-		for (let f of ['onerror', 'ontimeout'])
-			opt[f] = reject
-		*/
- 
-		opt.onerror = opt.ontimeout = reject
-		opt.onload = resolve
- 
-        // console.log('Request', opt)
- 
-		GM_xmlhttpRequest(opt)
-	}).then(res => {
-        if (res.status === 200) return Promise.resolve(res.response)
-        else return Promise.reject(res)
-    }, err => {
-        return Promise.reject(err)
-    })
+function Request(url, opt = {}) {
+  Object.assign(opt, {
+    url,
+    timeout: 2000,
+    responseType: 'json'
+  })
+
+  return new Promise((resolve, reject) => {
+    /*
+    for (let f of ['onerror', 'ontimeout'])
+      opt[f] = reject
+    */
+
+    opt.onerror = opt.ontimeout = reject
+    opt.onload = resolve
+
+    // console.log('Request', opt)
+
+    GM_xmlhttpRequest(opt)
+  }).then(res => {
+    if (res.status === 200) return Promise.resolve(res.response)
+    else return Promise.reject(res)
+  }, err => {
+    return Promise.reject(err)
+  })
 }
- 
-function Get(url, opt={}) {
-    Object.assign(opt, {
-        method: 'GET'
-    })
-    return Request(url, opt)
+
+function Get(url, opt = {}) {
+  Object.assign(opt, {
+    method: 'GET'
+  })
+  return Request(url, opt)
 }
- 
-function Post(url, opt={}) {
-    Object.assign(opt, {
-        method: 'POST'
-    })
-    return Request(url, opt)
+
+function Post(url, opt = {}) {
+  Object.assign(opt, {
+    method: 'POST'
+  })
+  return Request(url, opt)
 }
- 
+
 function showToast(msg, doNotFade) {
-    let style = `position: fixed; right: 10px; top: 80px; width: 300px; text-align: left; background-color: rgba(255, 255, 255, 0.9); z-index: 99; padding: 10px 20px; border-radius: 5px; color: #222; box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2); font-weight: bold;`
- 
-    let span = document.createElement('span')
-    span.setAttribute('style', style)
-    span.innerText = msg
-    document.body.appendChild(span)
-    if (!doNotFade) {
-        setTimeout(() => {
-            document.body.removeChild(span)
-        }, 5000)
+  let style = `position: fixed; right: 10px; top: 80px; width: 300px; text-align: left; background-color: rgba(255, 255, 255, 0.9); z-index: 99; padding: 10px 20px; border-radius: 5px; color: #222; box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2); font-weight: bold;`
+
+  let span = document.createElement('span')
+  span.setAttribute('style', style)
+  span.innerText = msg
+  document.body.appendChild(span)
+  if (!doNotFade) {
+    setTimeout(() => {
+      document.body.removeChild(span)
+    }, 5000)
+  }
+}
+
+async function GetElementByText(startElem, selector, text, exist) {
+  /*
+  selector: 选择器
+  text: 内容
+  exist: 是否只要存在就ojbk
+  */
+  exist = exist || false
+  let elemList = startElem.querySelectorAll(selector)
+  for (let i = 0; i < elemList.length; ++i) {
+    let elem = elemList[i]
+    if (exist) {
+      if (elem.innerText.search(text) !== -1) return elem
+    } else {
+      if (elem.innerText === text) return elem
     }
+  }
 }
---
+++ scripts/让我直接跳转好吗.js
@@ -0,0 +1,100 @@
+// ==UserScript==
+// @name         让我直接跳转好吗
+// @namespace    https://greasyfork.org/zh-CN/users/177458-bd777
+// @version      0.4
+// @description  网页中的外链直接跳出去而不再被拦一道
+// @author       windeng
+// @match        *://*.zhihu.com/*
+// @match        *://c.pc.qq.com/*
+// @match        *://*.jianshu.com/*
+// @grant        none
+// ==/UserScript==
+
+function gaoZhihu() {
+  if (!window.location.href.match('zhihu.com')) return
+
+  let run = () => {
+    let cnt = 0
+    let aList = document.querySelectorAll('a')
+    for (let a of aList) {
+      const href = a.getAttribute('href')
+      const matches = href.match(/link\.zhihu\.com\/?\?target=(.*)/)
+      // console.log('?', href, matches)
+      if (matches) {
+        const url = decodeURIComponent(matches[1])
+        a.setAttribute('href', url)
+        // console.log(`${href} => ${url}`)
+        ++cnt
+      }
+    }
+    return cnt > 0
+  }
+
+  run()
+
+  let MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
+
+  let observer = new MutationObserver(function (mutations) {
+    run()
+  })
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  })
+}
+
+function gaoQQ() {
+  if (!window.location.href.match('c.pc.qq.com')) return
+
+  const matches = window.location.search.match(/pfurl=([^&$]*)/)
+  if (matches) {
+    let url = decodeURIComponent(matches[1])
+    console.log('QQ url', url)
+    if (!url.startsWith('http') && !url.startsWith('//')) url = '//' + url
+    window.location.href = url
+  }
+}
+
+function gaoJianshu() {
+  if (!window.location.href.match('jianshu.com')) return
+
+  let run = () => {
+    let cnt = 0
+    let aList = document.querySelectorAll('a')
+    for (let a of aList) {
+      const href = a.getAttribute('href')
+      const matches = href.match(/links.jianshu.com\/go\?to=([^&$]*)/)
+      // console.log('?', href, matches)
+      if (matches) {
+        const url = decodeURIComponent(matches[1])
+        a.setAttribute('href', url)
+        // console.log(`${href} => ${url}`)
+        ++cnt
+      }
+    }
+    return cnt > 0
+  }
+
+  run()
+
+  let MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
+
+  let observer = new MutationObserver(function (mutations) {
+    run()
+  })
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  })
+}
+
+(function () {
+  'use strict';
+
+  // Your code here...
+  gaoZhihu()
+  gaoQQ()
+  gaoJianshu()
+})();
