5a71b14bd4658cbe718bc3897d720cb6600153eb
windeng <mis_tletoe@foxmail.com>
2021-10-14 17:39:00 +0800

    update

--- library/SimpleTools.js
+++ library/SimpleTools.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         个人使用的一些简单函数，勿安装
+// @namespace    https://greasyfork.org/zh-CN/users/177458-bd777
+// @version      0.1
+// @description  个人使用的一些简单函数，勿安装，详情查看代码中的函数说明
+// @author       windeng
+// @grant        none
+// ==/UserScript==
+
+
 // just sleep for some seconds
 async function Sleep(sleepSecs) {
   return new Promise((resolve, reject) => {
